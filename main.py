b205ba44440ae611375a00dca08c7d0a78501cbc
Francisco Vitor Gomes Castro <francisco.fvg001@gmail.com>
2023-10-07 19:53:43 -0300

    Intengrando com gofile api

--- main.py
+++ main.py
@@ -14,6 +14,21 @@ headers = {
         'x-requested-with': 'XMLHttpRequest'
     }
 
+def get_gofile_server():
+    url = "https://api.gofile.io/getServer"
+    response = requests.get(url, data={})
+    return response.json().get("data").get("server")
+
+def upload_to_gofile_and_get_link(pdfPath):
+    url = f"https://{get_gofile_server()}.gofile.io/uploadFile"
+    
+    files = {
+        'file': open(pdfPath, 'rb')
+    }
+    
+    response = requests.post( url, files=files, data={})
+    return response.json().get("data").get("downloadPage")
+
 def search_manga(name):
     url = "https://mangalivre.net/lib/search/series.json"
 
@@ -85,8 +100,15 @@ def save_chapter_pages(manga_name, chapter_number, pages):
         else:
             print(f"Erro ao baixar página {page} - " + response.text)
             
-    
     c.save()
+    
+    print('Criando link de download...')
+    download_link = upload_to_gofile_and_get_link(pdfPath)
+    print(f'link de download (Capitulo {chapter_number}): {download_link}')
+    
+    #os.remove(pdfPath)
+    
+    return chapter_number, download_link
 
 
 def main():
@@ -114,6 +136,8 @@ def main():
             chapter2 = int(chapter)
         print(f'Procurando capítulos de {chapter} a {chapter2}...')
         
+        download_links = []
+        
         for chapter in range(int(chapter), int(chapter2) + 1):
             id_release = get_chapter(id_serie, str(chapter))
             pages = get_page(id_release)
@@ -121,8 +145,11 @@ def main():
             print(json.dumps(pages, indent=4))
             print("Baixando...")
 
-            save_chapter_pages(name, str(chapter), pages)
+            download_links.append(save_chapter_pages(name, str(chapter), pages))
             print("Capítulo baixado com sucesso!")
+            
+        for download_link in download_links:
+            print(f"Link de download (Capítulo {download_link[0]}): {download_link[1]}")
 
 
 if __name__ == "__main__":
